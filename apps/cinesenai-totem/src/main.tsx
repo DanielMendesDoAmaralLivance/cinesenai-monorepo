c43c91b12df1e9096fd8a183f744537ca1f7285f
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-06-08 11:35:27 -0300

    Configura shadcnui

--- apps/cinesenai-totem/src/main.tsx
+++ apps/cinesenai-totem/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from '@/components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 className='bg-amber-400'>Oi</h1>
+    <Button className='bg-amber-400'>Oi</Button>
   </StrictMode>
 )
