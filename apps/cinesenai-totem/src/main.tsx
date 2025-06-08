9bc39e195e54067b8f4216b3fe845a8280574d3b
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-06-08 11:46:59 -0300

    Termina de configurar shadcnui

--- apps/cinesenai-totem/src/main.tsx
+++ apps/cinesenai-totem/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Button } from '@/components/ui/button'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Button className='bg-amber-400'>Oi</Button>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Button>Oi</Button>
+    </ThemeProvider>
   </StrictMode>
-)
+);
