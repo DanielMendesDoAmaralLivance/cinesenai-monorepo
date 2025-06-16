ad5ff5d6d8c24e15401d2a721a1f42eaf61f2393
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-06-16 10:10:32 -0300

    Adiciona animação no card de sessão

--- apps/cinesenai-totem/src/components/sessao-card.tsx
+++ apps/cinesenai-totem/src/components/sessao-card.tsx
@@ -1,19 +1,28 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "./ui/badge";
+import { Link } from "@tanstack/react-router";
+import { motion } from "framer-motion";
 
 export const SessaoCard = () => {
   return (
-    <Card className="rounded-lg min-h-[125px] w-[190px] col-span-1">
-      <CardContent className="px-4 flex flex-col justify-between">
-        <div className="min-h-[50px] text-sm font-medium">
-          <h1>Ter, 08 Jul, 20h00</h1>
-        </div>
+    <Link>
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        transition={{ type: "spring", stiffness: 300, damping: 15 }}
+      >
+        <Card className="rounded-lg min-h-[125px] w-[190px] col-span-1">
+          <CardContent className="px-4 flex flex-col justify-between">
+            <div className="min-h-[50px] text-sm font-medium">
+              <h1>Ter, 08 Jul, 20h00</h1>
+            </div>
 
-        <div className="flex items-center flex-wrap">
-          <Badge className="mr-1">Sala 1</Badge>
-          <Badge>Legendado</Badge>
-        </div>
-      </CardContent>
-    </Card>
+            <div className="flex items-center flex-wrap">
+              <Badge className="mr-1">Sala 1</Badge>
+              <Badge>Legendado</Badge>
+            </div>
+          </CardContent>
+        </Card>
+      </motion.div>
+    </Link>
   );
 };
