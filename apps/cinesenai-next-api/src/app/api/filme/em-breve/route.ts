24f5b21dd95afbfc9bf22a24f7da58fb1a1d9130
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-11-08 10:52:54 -0300

    Fix: Corrige paths para o deploy

--- apps/cinesenai-next-api/src/app/api/filme/em-breve/route.ts
+++ apps/cinesenai-next-api/src/app/api/filme/em-breve/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import prisma from "../../../../lib/prisma";
 import type { FilmeComGeneros } from "@cinesenai-monorepo/types-custom";
 
 export async function GET(
--- apps/cinesenai-next-api/src/app/api/ingresso/route.ts
+++ apps/cinesenai-next-api/src/app/api/ingresso/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import prisma from "../../../lib/prisma";
 import { Prisma } from "@cinesenai-monorepo/types";
 
 export async function POST(request: NextRequest) {
